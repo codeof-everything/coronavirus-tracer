8b8a7b594a444f1a97e9ba0a0e60ee4a38d7619f
codeof-everything <codeofeverything@gmail.com>
2020-03-27 20:46:05 -0400

    Commiting initial spring boot project

---
+++ coronavirus-tracer/src/test/java/com/codeof/everything/coronavirustracer/CoronavirusTracerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeof.everything.coronavirustracer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTracerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
